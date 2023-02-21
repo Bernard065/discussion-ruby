5542300275205d26a9c76221900d34debd0747b6
Bernard065 <bebenibernard@gmail.com>
2023-02-21 11:00:25 +0300

    discussion questions

--- work.rb
+++ work.rb
@@ -13,6 +13,13 @@ class Author
        book
      end
 
+     def list_books
+        puts "Books by #{@name}:"
+        @books.map do |book|
+          puts "- #{book.title}"
+        end
+    end
+
 end
 
 
@@ -36,10 +43,14 @@ book2 = author1.add_book("The Last of Us")
 book3 = author1.add_book("Ruby Phase 3")
 book4 = author2.add_book("Ego is an Enemy")
 book5 = author3.add_book("5Am Club")
-puts "#{author1.name} has written #{book1.title}"
-puts "#{author1.name} has written #{book2.title}"
-puts "#{author1.name} has written #{book3.title}"
-puts "#{author2.name} has written #{book4.title}"
-puts "#{author3.name} has written #{book5.title}"
+puts author1.list_books
+puts author2.list_books
+puts author3.list_books
+
+# puts "#{author1.name} has written #{book1.title}"
+# puts "#{author1.name} has written #{book2.title}"
+# puts "#{author1.name} has written #{book3.title}"
+# puts "#{author2.name} has written #{book4.title}"
+# puts "#{author3.name} has written #{book5.title}"
 
 
